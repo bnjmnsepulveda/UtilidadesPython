77b83b29640c051c28dc4f4321efaedb16c2298b
benjamin <bnjmnsepulveda@gmail.com>
2019-04-22 16:59:24 -0400

    se mejora cliente_postgres.py, cierre correcto de recursos

--- modulos/conexion_postgres.py
+++ modulos/conexion_postgres.py
@@ -1,21 +1,4 @@
 import psycopg2
-from datasource.model import Dialogo
-
-
-class Dialogo:
-
-    def __init__(self, pregunta=None, respuesta=None):
-        self.pregunta = pregunta
-        self.respuesta = respuesta
-
-    def __str__(self):
-        return '[' + self.pregunta + ' - ' + self.respuesta + ']'
-
-def adapter(row):
-    dialogo = Dialogo()
-    dialogo.pregunta = row[1]
-    dialogo.respuesta = row[2]
-    return dialogo
 
 
 class ConexionPostgres:
@@ -32,33 +15,45 @@ class ConexionPostgres:
 
     def select_all(self, sql, adapter_function, *parametros):
         registros = []
-        with self.connect() as cx:
-            with cx.cursor() as cursor:
+        cx = self.connect()
+        try:
+            cursor = cx.cursor()
+            try:
                 cursor.execute(sql, parametros)
                 rows = cursor.fetchall()
                 for row in rows:
                     registros.append(adapter_function(row))
-
+            finally:
+                cursor.close()
+        finally:
+            cx.close()
         return registros
 
     def select_one(self, sql, adapter_function, *parametros):
-        registro = {}
-        with self.connect() as cx:
-            with cx.cursor() as cursor:
+        registro = None
+        cx = self.connect()
+        try:
+            cursor = cx.cursor()
+            try:
                 cursor.execute(sql, parametros)
-                rows = cursor.fetchone()
-                for row in rows:
+                row = cursor.fetchone()
+                if row:
                     registro = adapter_function(row)
-
+            finally:
+                cursor.close()
+        finally:
+            cx.close()
         return registro
 
     def update(self, sql, *parametros):
-        with self.connect() as cx:
-            with cx.cursor() as cursor:
+        cx = self.connect()
+        try:
+            cursor = cx.cursor()
+            try:
                 cursor.execute(sql, parametros)
+            finally:
+                cursor.close()
+        finally:
+            cx.close()
 
 
-conexion = ConexionPostgres(host='localhost', basedatos='cckall_chatbot', usuario='postgres', clave='adp2019')
-regs = conexion.select_all('SELECT * FROM dialogo', adapter)
-for r in regs[0:10]:
-    print(r.pregunta)
